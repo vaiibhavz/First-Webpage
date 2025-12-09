0810dcfbaa126b24c75b732e634e270eec627d50
vaiibhavz <vaibhavch99z@gmail.com>
2025-12-10 00:26:32 +0530

    Add website files

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+document.getElementById("Button").onclick = function (){
+    alert("This feature is under development.");
+};
+
+document.getElementById("alertButton").onclick = function (){
+    alert("Hello! your clicked the button.");
+};
